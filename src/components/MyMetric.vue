5e3c3f41f93b00dc62c7a96ae5bfae8966ec8141
vbutov <vadimvyatich@gmail.com>
2022-06-08 13:16:30 +0300

    [FIX] add function compute percent

--- src/components/MyMetric.vue
+++ src/components/MyMetric.vue
@@ -1,74 +1,70 @@
 <template>
     <div>
         <h2>Статистика состояний объектов мониторинга</h2>
-        <div>
+        <div class="percents"> 
             <ul>
                 <li>Всего: {{get100Procent()}}</li>
-                <li  v-for="e in metric()" :key="e">
-                    <div :class="e.state"></div>
-                    {{e.state}} {{ getProcent(e)* 100}}%
-                </li>
+                 <div class="row">
+                    <li  v-for="e in metric()" :key="e">
+                        <span :class="e.state"></span>
+                        {{e.state}} {{e.count }}
+                    </li>
+                </div>
             </ul>
         </div>
+        <div class="chart">
+                    <my-test :metric="procents()"></my-test>
+            </div>
     </div>
 </template>
 <script setup lang="ts">
 import {a} from '@/ListObject'
 import { computed } from 'vue'
+import MyTest from '@/components/MyTest.vue'
     const listJSON = JSON.stringify(a)
-    // получил массив
     const allStatuses = new Set(JSON.parse(listJSON).map(data => (data.status)))
+    // здесь хранится массив статусов для дальнешего подсчета записей  
     const list = JSON.parse(listJSON).map(data => ({"status":data.status}))
-    // const list1 = JSON.parse(listJSON).map(data => ({"status":data.status,  "count": 0 }))
+    // получаем метрику 
     const metric = () => {
         let result = []
         allStatuses.forEach(state =>{
             let s = list.filter(e => e.status === state)
-            // console.log("state "+ s.length)
+
             result.push({"state" : state, "count": s.length })
         })
-        // console.log(result)
         return result
     }
     const get100Procent = () =>{ 
         let all = 0
         metric().forEach(e => {
-           // console.log(e)
             all +=e.count
-       //     console.log("this all ="+all)
         })
         return all
     }
-    const getProcent = (e) => {
-        console.log(e)
-        return ((get100Procent()/100) * e.count).toFixed(1)
-         }
-
-
-    // const mapList1= computed(() => {
-    //   let result:object[] = [] 
-    //   list1.forEach(e => {
-    //       console.log("Содержимое массива " + JSON.stringify(result))
-    //     let find = ((e) => {
-    //         if(result.length < 0 ){
-    //             return false
-    //         }
-    //         for(let i in result) {
-    //              console.log("e= " + e.status);
-    //              if( i.status === e.status){
-    //                  console.log(result[i])
-    //                  return true
-    //              }
-    //         }
-    //         return false
-    //         })
-    //     console.log("find "+ find(e))
-    //     console.log("indexOF" + list1.indexOf("status"))
-    //      console.log("=========================")
-    
-    //   }) 
-    //   return result 
-    // })
+    // const getProcent = (e) => ((get100Procent()/100) * e.count).toFixed(1)
+    // получаем массив с содержащий цвета и процент 
+    const procents = () => {
+        let procent = []
+        metric().forEach((e) =>{
+            let data = {}
+                data.status = e.state,
+                data.color = getColor(e.state)
+                data.procent = ((e.count/get100Procent()) * 100).toFixed(1) 
+            procent.push(data)
+        })
+        return procent
+    }
+  const getColor =(e) => {
+      let x = "black"
+      switch(e){
+          case "critical" : x=  "brown"; break
+          case "ok" : x = "green"; break
+          case "error" : x=  "red"; break
+          case "warning" : x=  "yellow"; break
+      }
+      return x
+  } 
  
 </script>
 <style>
@@ -76,6 +72,7 @@ import { computed } from 'vue'
      list-style-type:none;
  }
  .ok{
+     display: inline-block;
      width: 15px;
      height: 15px;
      border: 2px solid green;
@@ -83,6 +80,7 @@ import { computed } from 'vue'
      background-color:green;
  }
   .error{
+       display: inline-block;
     width: 15px;
      height: 15px;
      border: 2px solid red;
@@ -90,6 +88,7 @@ import { computed } from 'vue'
      background-color: red;
  }
   .warning{
+       display: inline-block;
     width: 15px;
      height: 15px;
      border: 2px solid yellow;
@@ -97,13 +96,26 @@ import { computed } from 'vue'
      background-color: yellow;
  }
  .critical{
+      display: inline-block;
       width: 15px;
      height: 15px;
      border: 2px solid brown;
      border-radius: 50%; 
      background-color: brown;
  }
- .circle{
-
+ .row{
+      display: inline-block;
+     display: inline-block;
+     text-align: left;
+     width: 50%;
+ }
+ .percents{
+     display: inline-block;
+     vertical-align: top;
+     width: 50%;
+ }
+ .chart{
+      display: inline-block;
+       width: 50%;
  }
 </style>
